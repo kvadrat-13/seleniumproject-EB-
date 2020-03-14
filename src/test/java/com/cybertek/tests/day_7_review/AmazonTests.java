d4e14b21f6891e38ca2cdbeeb0637b9b4a015a0a
Yourname <YourEmail>
2020-03-14 10:54:05 -0400

    update from home

---
+++ src/test/java/com/cybertek/tests/day_7_review/AmazonTests.java
@@ -0,0 +1,23 @@
+package com.cybertek.tests.day_7_review;
+
+public class AmazonTests {
+    public static void main(String[] args) {
+        nameMatchTest();
+    }
+
+    // each test will be a seperate method
+
+    /**
+     * open browser
+     * got to amazon
+     * search for nay item
+     * rememeber the name of the first result
+     * click on the first result
+     * verify that product name is same in the product page
+     *
+     */
+
+    private static void nameMatchTest(){
+
+    }
+}
